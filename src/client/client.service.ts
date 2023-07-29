a34c8fc18423a8ad7266b74ec84b4b082fc4ef03
Sunaidy <sjauregui@refcfg.cu>
2023-07-28 21:37:57 -0400

    add mongo

--- src/client/client.service.ts
+++ src/client/client.service.ts
@@ -1,17 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { UpdateClientDto } from './dto/update-client.dto';
 import { client } from './interfaces/client.interface';
+import { Client } from './dto/client.sheme';
+import { Model } from 'mongoose';
+import { CreateClientDto } from './dto/create-client.dto';
+import { throws } from 'assert';
 
 @Injectable()
 export class ClientService {
+  constructor(@InjectModel(Client.name) private clientModel: Model<Client>) {}
   private clients: client[] = [];
 
-  create(client: client) {
-    this.clients.push(client);
+  create(createClientDto: CreateClientDto) {
+    try{
+      const createdClient = new this.clientModel(createClientDto);
+      return createdClient.save();
+    } catch(error){
+      throw new HttpException('Failed to create client', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-  findAll(): client[] {
-    return this.clients;
+  findAll(): Promise<Client[]> {
+    return this.clientModel.find().exec();
   }
 
   findOne(id: number) {
@@ -19,12 +30,16 @@ export class ClientService {
   }
 
   update(id: number, updateClientDto: UpdateClientDto) {
+    this.clientModel.findByIdAndUpdate(id,);
     this.clients = this.clients.map((obj)=>obj.id == id ? {...obj, ...updateClientDto}:obj )
     return this.clients;
   }
 
-  remove(id: number) {
-    this.clients = this.clients.filter((obj)=> {return obj.id != id })
-    return this.clients;
+ async remove(id: string): Promise<CreateClientDto> {
+    try{
+      return await this.clientModel.findByIdAndDelete(id);
+    } catch(error){
+      throw new HttpException('Failed to delete client' + error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
